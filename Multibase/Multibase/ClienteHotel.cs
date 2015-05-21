9a98092234d29ac7e9b23face5fae5d0738551dd
Diana Saur <dianasaurio@gmail.com>
2015-05-21 01:59:09 -0500

    User controls!

---
+++ Multibase/Multibase/ClienteHotel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multibase
+{
+    public class ClienteHotel
+    {
+   
+        public string nombre{ get; set; }
+        public string apPat { get; set; }
+        public string apMat { get; set; }
+
+        public ClienteHotel(string n, string ap, string am) {
+
+            nombre = n;
+            apPat = ap;
+            apMat = am;
+
+        }
+    }
+}
---
+++ Multibase/Multibase/Clientes.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace Multibase
+{
+    /// <summary>
+    /// Interaction logic for Clientes.xaml
+    /// </summary>
+    public partial class Clientes : UserControl
+    {
+        public ObservableCollection<ClienteHotel> clientesHotelList { get; set; }
+        public Clientes()
+        {
+            InitializeComponent();
+            clientesHotelList = new ObservableCollection<ClienteHotel>();
+            clientesHotelList.Add(new ClienteHotel("Pancho", "Pérez", "Pérez"));
+            clientesHotelList.Add(new ClienteHotel("Karla", "López", "López"));
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
